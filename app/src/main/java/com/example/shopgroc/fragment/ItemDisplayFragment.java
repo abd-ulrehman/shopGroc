3395141f0a15ca5ac0a47a45247103ec70d5fb59
rehma <abd_ul.rehman@yahoo.com>
2020-05-09 17:39:35 +0500

    item open page design cart page design

--- app/src/main/java/com/example/shopgroc/fragment/ItemDisplayFragment.java
+++ app/src/main/java/com/example/shopgroc/fragment/ItemDisplayFragment.java
@@ -1,42 +1,39 @@
 package com.example.shopgroc.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.example.shopgroc.R;
-import com.example.shopgroc.interfaces.ChildToParentCallback;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
+ * Use the {@link ItemDisplayFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DashboardFragment extends Fragment implements View.OnClickListener {
+public class ItemDisplayFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    LinearLayout itemCardView;
-    NavController navigationController;
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    ChildToParentCallback varChildToParentCallback;
 
-    public DashboardFragment() {
+    Button btnPlus, btnNimus;
+    TextView counter;
+    int itemCount, maxLength = 10 , minLength = 0;
+
+    public ItemDisplayFragment() {
         // Required empty public constructor
     }
 
@@ -46,11 +43,11 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
+     * @return A new instance of fragment ItemDisplayFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
+    public static ItemDisplayFragment newInstance(String param1, String param2) {
+        ItemDisplayFragment fragment = new ItemDisplayFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -71,28 +68,40 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
-    }
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        varChildToParentCallback = (ChildToParentCallback)context;
-        varChildToParentCallback.hideBottomNav(false);
+        return inflater.inflate(R.layout.fragment_item_display, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        itemCardView = view.findViewById(R.id.itemId);
         InIt(view);
     }
 
     private void InIt(View view) {
-        navigationController = Navigation.findNavController(view);
-        itemCardView.setOnClickListener(this);
+        btnNimus = view.findViewById(R.id.buttonMinus);
+        btnPlus = view.findViewById(R.id.buttonPlus);
+        counter = view.findViewById(R.id.itemCount);
+        btnPlus.setOnClickListener(this);
+        btnNimus.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        navigationController.navigate(R.id.action_navigation_dashboard_to_itemDisplayFragment);
+        int id = v.getId();
+        if(id == R.id.buttonPlus){
+            maxLength = Integer.parseInt(counter.getText().toString());
+            if(maxLength < 10) {
+                itemCount = itemCount + 1;
+                counter.setText(Integer.toString(itemCount));
+            }
+        }
+        else if(id == R.id.buttonMinus){
+            minLength = Integer.parseInt(counter.getText().toString());
+            if(minLength >= 1) {
+                itemCount = itemCount - 1;
+                counter.setText(Integer.toString(itemCount));
+            }
+        }
     }
 }
