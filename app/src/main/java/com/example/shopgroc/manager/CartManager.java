98532ad1b0adffed67662441f5c34fe3dfe23c0a
rehma <abd_ul.rehman@yahoo.com>
2020-05-12 18:09:33 +0500

    Added CartManager with all functionalities CartItem

--- app/src/main/java/com/example/shopgroc/manager/CartManager.java
+++ app/src/main/java/com/example/shopgroc/manager/CartManager.java
@@ -1,12 +1,44 @@
 package com.example.shopgroc.manager;
 
+import com.example.shopgroc.model.CartItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartManager {
     private static CartManager instance = null;
+    private List<CartItem> cartItemList = new ArrayList<>();
+
     private CartManager(){}
     public static CartManager getInstance(){
         if(instance==null){
             instance = new CartManager();
         }
         return instance;
+    }
+    public void addToCart(CartItem item){
+        cartItemList.add(item);
+    }
+    public void removeItem(CartItem item){
+        for(int i=0; i<cartItemList.size(); i++){
+            if(item.getProduct().getId() == cartItemList.get(i).getProduct().getId()){
+                cartItemList.remove(i);
+                break;
+            }
+        }
+    }
+    public void updateItem(CartItem item){
+        for(int i=0;i<cartItemList.size(); i++){
+            if(item.getProduct().getId() == cartItemList.get(i).getProduct().getId()){
+                cartItemList.get(i).setQuantity(item.getQuantity());
+                break;
+            }
+        }
+    }
+    public List<CartItem> getItemList(){
+        return cartItemList;
+    }
+    public int getCartItemCount(){
+        return cartItemList.size();
     }
 }
